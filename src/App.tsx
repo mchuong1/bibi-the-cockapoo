d39968f2ec3a5d56b28b58184427f29c11e32171
Matthew Chuong <mstchuong@yahoo.com>
2023-04-25 22:18:42 -0400

    remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from 'react';
 import styles from './App.module.scss';
 import { LandingPage } from './components/landing-page/landing-page';
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
         <div className={styles.App}>
             <LandingPage />
